59d8d0b67c27b0a6bbf503044d2e2e793489a002
webmaster540 <webmaster@rocnation.com>
2026-04-06 18:31:05 -0400

    only walkman view

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import DisplayToggle from "./components/DisplayToggle";
-import Winamp from "./components/Winamp";
+// import { useState } from "react";
+// import DisplayToggle from "./components/DisplayToggle";
+// import Winamp from "./components/Winamp";
 import Walkman from "./components/Walkman";
 
 export default function Home() {
-  const [displayMode, setDisplayMode] = useState<"winamp" | "walkman">(
-    "winamp",
-  );
+  // const [displayMode, setDisplayMode] = useState<"winamp" | "walkman">(
+  //   "winamp",
+  // );
 
-  const toggleMode = () => {
-    setDisplayMode((prev) => (prev === "winamp" ? "walkman" : "winamp"));
-  };
+  // const toggleMode = () => {
+  //   setDisplayMode((prev) => (prev === "winamp" ? "walkman" : "winamp"));
+  // };
 
   return (
-    <main className="relative w-full h-screen bg-black overflow-hidden">
-      {/* The Toggle Button */}
-      <DisplayToggle mode={displayMode} onToggle={toggleMode} />
+    <Walkman />
+    // <main className="relative w-full h-screen bg-black overflow-hidden">
+    //   {/* The Toggle Button */}
+    //   <DisplayToggle mode={displayMode} onToggle={toggleMode} />
 
-      Conditional Rendering
-      <div className="w-full h-full flex items-center justify-center">
-        {displayMode === "winamp" ? <Winamp /> : <Walkman />}
-      </div>
-    </main>
+    //   Conditional Rendering
+    //   <div className="w-full h-full flex items-center justify-center">
+    //     {displayMode === "winamp" ? <Winamp /> : <Walkman />}
+    //   </div>
+    // </main>
   );
 }
